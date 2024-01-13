9d033364384cee0d0b24cfa24faa2d064390e1b4
andi muh haikal lukman <andimuhhaikal79@gmail.com>
2024-01-14 00:15:59 +0800

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,14 +1,19 @@
-<
-!DOCTYPE html>
+< !DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CoreCatalyst</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <link rel="stylesheet" href="{{ asset('frontend/assets/style.css') }}" />
 </head>
+
 <body>
 
+
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 </body>
+
 </html>
