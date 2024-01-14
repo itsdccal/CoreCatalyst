7ecd5119ffd5be8d7c2cb92c1bb2fdeb8373bda2
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-01-15 00:55:50 +0800

    fix: hapus tabel muscletrained

--- app/Models/Exercise.php
+++ app/Models/Exercise.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Exercise extends Model
 {
     use HasFactory;
+
+    public function primaryMuscle() {
+        return $this->belongsTo(Muscle::class, 'primaryMuscleTrained');
+    }
+
+    public function secondaryMuscle() {
+        return $this->belongsTo(Muscle::class, 'secondaryMuscleTrained');
+    }
+
+    public function tertiaryMuscle() {
+        return $this->belongsTo(Muscle::class, 'tertiaryMuscleTrained');
+    }
 }
