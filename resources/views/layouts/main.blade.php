a31204a3f68b2c65c903daf2ab1dc780f36cef0f
andi muh haikal lukman <andimuhhaikal79@gmail.com>
2024-01-15 00:04:21 +0800

    setup tailwindcss

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,7 @@
     <title>CoreCatalyst</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
     <link rel="stylesheet" href="{{ asset('frontend/assets/style.css') }}" />
+    @vite('resources/css/app.css')
 </head>
 
 <body>
