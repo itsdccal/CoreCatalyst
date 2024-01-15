b064df0b0b7731381bd59b399e24a1c220ccadf3
Ruukaze <joyabrianrantepasang14@gmail.com>
2024-01-15 15:26:38 +0800

    feat: tambah equipment seeder

--- database/seeders/EquipmentSeeder.php
+++ database/seeders/EquipmentSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Equipment;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,40 @@ class EquipmentSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Equipment::create([
+            "name" => "Barbell",
+        ]);
+
+        Equipment::create([
+            "name" => "Dumbbell",
+        ]);
+
+        Equipment::create([
+            "name" => "Kettlebell",
+        ]);
+
+        Equipment::create([
+            "name" => "Machine",
+        ]);
+
+        Equipment::create([
+            "name" => "Plate",
+        ]);
+
+        Equipment::create([
+            "name" => "Resistance Band",
+        ]);
+
+        Equipment::create([
+            "name" => "Suspension",
+        ]);
+
+        Equipment::create([
+            "name" => "Bodyweight",
+        ]);
+
+        Equipment::create([
+            "name" => "Other",
+        ]);
     }
 }
